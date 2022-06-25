cd5d0e0a3ab4d0355dedcfd98655e9aabcbbe0cd
ionut.marginean <ionut.marginean01@gmail.com>
2022-06-25 09:23:44 +0300

    123

---
+++ person-server/src/test/java/org/afsttrackit/person/server/PersonServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.afsttrackit.person.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
